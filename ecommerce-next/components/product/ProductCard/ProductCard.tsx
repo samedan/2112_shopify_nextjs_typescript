b39c9388bd342bcf5bc0dc93a554482e733630e8
Daniel Popescu <same.dan@gmail.com>
2021-12-19 11:41:34 +0100

    added pseudoclasses on the css

--- ecommerce-next/components/product/ProductCard/ProductCard.tsx
+++ ecommerce-next/components/product/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from "@common/types/product";
 import { FC } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import s from "./ProductCard.module.css";
 
 interface Props {
   product: Product;
@@ -12,15 +13,17 @@ const placeholderImage = "/product-image-placeholder.svg";
 const ProductCard: FC<Props> = ({ product }) => {
   return (
     <Link href={`/products/${product.slug}`}>
-      <a>
-        <div>
-          <h3>
+      <a className={s.root}>
+        <div className={s.productBg}></div>
+        <div className={s.productTag}>
+          <h3 className={s.productTitle}>
             <span>{product.name}</span>
           </h3>
-          <span>14$</span>
+          <span className={s.productPrice}>14$</span>
         </div>
         {product.images && (
           <Image
+            className={s.productImage}
             alt={product.name ?? "Product image"}
             src={product.images[0].url ?? placeholderImage}
             height={540}

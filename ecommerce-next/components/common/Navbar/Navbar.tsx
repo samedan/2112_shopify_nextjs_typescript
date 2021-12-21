40a8b63581353ffc61ec6d8a08113d2050622213
Daniel Popescu <same.dan@gmail.com>
2021-12-21 12:04:16 +0100

    added menu items css

--- ecommerce-next/components/common/Navbar/Navbar.tsx
+++ ecommerce-next/components/common/Navbar/Navbar.tsx
@@ -1,11 +1,35 @@
 import { FC } from "react";
 import { Container } from "@components/ui";
+import Link from "next/link";
+import s from "./Navbar.module.css";
+import { Usernav } from "@components/common";
 
 const Navbar: FC = () => {
   return (
     <Container>
-      <div className="flex flex-row md:py-6">
-        <h1>NavBar</h1>
+      <div className={s.root}>
+        <div className="flex flex-1 items-center">
+          <Link href="/">
+            <a className={s.logo}>NEXT_STORE</a>
+          </Link>
+          <nav className="ml-6 space-x-6">
+            <Link href="/">
+              <a className={s.link}>All</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Clothes</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Accesories</a>
+            </Link>
+            <Link href="/">
+              <a className={s.link}>Shoes</a>
+            </Link>
+          </nav>
+          <div className="flex flex-1 justify-end space-x-8">
+            <Usernav />
+          </div>
+        </div>
       </div>
     </Container>
   );

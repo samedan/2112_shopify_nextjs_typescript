f0cca38b62a71aec0aada998b6ced77a842b7808
Daniel Popescu <same.dan@gmail.com>
2021-12-29 11:50:19 +0100

    Product Page added with Button

--- ecommerce-next/pages/products/[slug].tsx
+++ ecommerce-next/pages/products/[slug].tsx
@@ -6,6 +6,7 @@ import {
   InferGetStaticPropsType,
 } from "next";
 import { getAllProductsPaths, getProduct } from "@framework/product";
+import { ProductView } from "@components/product";
 
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -38,8 +39,7 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log(JSON.stringify(product, null, 2));
-  return <div>{JSON.stringify(product, null, 2)}</div>;
+  return <>{product && <ProductView product={product} />}</>;
 }
 
 ProductSlug.Layout = Layout;
